4fc4bf27dfc8a2ef93a0afc791b798d469794bbc
lingme <kid--l@hotmail.com>
2020-11-16 23:02:06 +0800

    update style

--- Client/index.js
+++ Client/index.js
@@ -1,18 +1,23 @@
-var loginClicked = function(){
-    alert("fff")
-};
-
-var logoutClicked = function(){
-    alert("fff")
-
-}
-
-var callPublicClicked = function(){
-    alert("fff")
-
-}
-
-var callProtectedClicked = function(){
-    alert("fff")
+$("#btn-login").click(function(){
+    var name = $("#in-name").val()
+    var pwd = md5($("#in-name").val())
     
-}
+    $.ajax({
+        url: "localhost:3000/api/login",
+        type: "post",
+        data: {
+            username: name,
+            password: pwd
+        },
+        dataType: "json",
+        success:function(data){
+            console.log(data)
+        }
+    })
+})
+
+$("#btn-public").click(function(){
+    $(".alert").delay(4000).slideUp(200, function() {
+        $(this).alert('close');
+    });
+})
