51ecc68d4541ad2a408032aefd637cced07966e9
lingme <kid--l@hotmail.com>
2020-11-16 18:33:35 +0800

    complete index style and add web client

---
+++ Client/index.js
@@ -0,0 +1,18 @@
+var loginClicked = function(){
+    alert("fff")
+};
+
+var logoutClicked = function(){
+    alert("fff")
+
+}
+
+var callPublicClicked = function(){
+    alert("fff")
+
+}
+
+var callProtectedClicked = function(){
+    alert("fff")
+    
+}
